cf245bfb69eea5e926653b8a2fe49f24014cb0af
rifandani <ipandani2505@gmail.com>
2023-10-19 22:26:33 +0700

    chore: bump react query, now with networkMode 'offlineFirst'

--- src/app/providers/query/queryClient.ts
+++ src/app/providers/query/queryClient.ts
@@ -3,7 +3,9 @@ import { QueryClient } from '@tanstack/react-query';
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1_000 * 30, // 30 secs. This will be the default in v5
+      // gcTime: 1_000 * 60 * 5, // 5 mins. Defaults to 5 mins
+      staleTime: 1_000 * 30, // 30 secs. Defaults to 0
+      networkMode: 'offlineFirst',
     },
   },
 });
